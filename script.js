cd2fa7157c407e4988c1da0f860c542bf53e67c0
greeyd-bite <greidy.agreda@cun.edu.co>
2025-04-28 21:02:45 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Agregar animaciones si es necesario, por ejemplo para los botones
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.1)';
+    });
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
